30940d2d40733a2bebc3b5a462f01bf2156cf36a
Mike Williams <mdub@dogbiscuit.org>
2013-06-23 15:45:10 +1000

    Pull out ScriptInjector.

--- lib/rack/livejs.rb
+++ lib/rack/livejs.rb
@@ -1,4 +1,4 @@
-require "rack/livejs/version"
+require "rack/livejs/script_injector"
 
 module Rack
   class Livejs
@@ -21,30 +21,6 @@ module Rack
         body = ScriptInjector.new(body)
       end
       [status, headers, body]
-    end
-
-    class ScriptInjector
-
-      def initialize(original_body, livejs_url = "http://livejs.com/live.js")
-        @original_body = original_body
-        @livejs_url = livejs_url
-      end
-
-      def each
-        original_body.each do |line|
-          line = line.sub(%r{(<head( [^>]*)?>)}i) { $1 + livejs_script_html }
-          yield line
-        end
-      end
-
-      private
-
-      attr_reader :original_body, :livejs_url
-
-      def livejs_script_html
-        %{<script src="#{livejs_url}"/>}
-      end
-
     end
 
   end
