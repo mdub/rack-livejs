7c2274f76a925b682872146721ab725bf8740ab3
Mike Williams <mdub@dogbiscuit.org>
2013-06-23 21:21:42 +1000

    Refactor.

--- lib/rack/livejs.rb
+++ lib/rack/livejs.rb
@@ -8,18 +8,14 @@ module Rack
     end
 
     def call(env)
-      if env["PATH_INFO"] == livejs_path
-        self.class.livejs_response
-      else
-        status, headers, body = @app.call(env)
-        if headers["Content-Type"] == "text/html"
-          html = ""; body.each { |chunk| html << chunk }
-          new_html = inject_livejs(html)
-          headers["Content-Length"] = new_html.size.to_s
-          body = [ new_html ]
-        end
-        [status, headers, body]
+      return livejs_response if env["PATH_INFO"] == livejs_path
+      status, headers, body = @app.call(env)
+      if headers["Content-Type"] == "text/html"
+        new_html = inject_livejs(read_body(body))
+        headers["Content-Length"] = new_html.size.to_s
+        body = [new_html]
       end
+      [status, headers, body]
     end
 
     private
@@ -28,26 +24,26 @@ module Rack
       "/_rack_livejs_/live.js"
     end
 
+    def read_body(body)
+      "".tap do |buffer|
+        body.each { |chunk| buffer << chunk }
+      end
+    end
+
     def inject_livejs(html)
       html.sub(%r{(<head( [^>]*)?>)}i) { $1 + %{<script src="#{livejs_path}"/>} }
     end
 
-    class << self
+    def livejs_javascript
+      @@livejs_javascript ||= ::File.read(::File.expand_path("../../../vendor/livejs/live.js", __FILE__))
+    end
 
-      def livejs_response
-        [
-          200,
-          { 'Content-Type' => "application/javascript", 'Content-Length' => livejs_javascript.size.to_s },
-          [ livejs_javascript ]
-        ]
-      end
-
-      def livejs_javascript
-        @livejs_javascript ||= begin
-          ::File.read(::File.expand_path("../../../vendor/livejs/live.js", __FILE__))
-        end
-      end
-
+    def livejs_response
+      [
+        200,
+        { 'Content-Type' => "application/javascript", 'Content-Length' => livejs_javascript.size.to_s },
+        [ livejs_javascript ]
+      ]
     end
 
   end
